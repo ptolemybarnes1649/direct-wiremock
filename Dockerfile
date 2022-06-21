9d75a766d2a077cfe8d334c06ec9cf7866cee3b4
Ptolemy Barnes <ptolemy.barnes@thoughtworks.com>
2022-06-21 12:00:25 +0200

    kinda works

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM public.ecr.aws/lambda/java:11
 
 COPY build/classes/java/main ${LAMBDA_TASK_ROOT}
-COPY build/dependency/* ${LAMBDA_TASK_ROOT}/lib/
+COPY build/dependency/* ${LAMBDA_TASK_ROOT}
 
 CMD [ "ptolemybarnes.Handler::handleRequest" ]
--- src/main/java/ptolemybarnes/Handler.java
+++ src/main/java/ptolemybarnes/Handler.java
@@ -2,10 +2,15 @@ package ptolemybarnes;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
-public class Handler implements RequestHandler<Object, String> {
+import java.util.Map;
+
+public class Handler implements RequestHandler<Map<String,String>, String> {
     @Override
-    public String handleRequest(Object input, Context context) {
+    public String handleRequest(Map<String,String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("REQUEST RECEIVED");
         return "test";
     }
 }
