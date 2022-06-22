57fb8f8440018ab1663111aefefe54b43ac15b0f
gc <ft7bcn@otto.de>
2022-06-22 18:52:33 +0200

    gc | Moving wiremock server to request handler. it works! | NOSTORY
    Co-authored-by: Gemma Cortel <gemma.cortel@thoughtworks.com>

--- src/main/java/ptolemybarnes/Handler.java
+++ src/main/java/ptolemybarnes/Handler.java
@@ -6,14 +6,39 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.Response;
+import com.github.tomakehurst.wiremock.direct.DirectCallHttpServer;
+import com.github.tomakehurst.wiremock.direct.DirectCallHttpServerFactory;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.github.tomakehurst.wiremock.http.RequestMethod.POST;
+import static ptolemybarnes.MockRequest.mockRequest;
+
 public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("REQUEST RECEIVED");
+        String inputPath = input.getPath();
+        logger.log("REQUEST RECEIVED " + inputPath);
+
+        DirectCallHttpServerFactory factory = new DirectCallHttpServerFactory();
+        var config = wireMockConfig().httpServerFactory(factory);
+        config.fileSource(new SingleRootFileSource("wiremock"));
+        WireMockServer wm = new WireMockServer(config);
+        wm.start(); // no-op, not required
+
+        DirectCallHttpServer server = factory.getHttpServer();
+
+        Request request = mockRequest().method(POST).url(inputPath);
+
+        Response response = server.stubRequest(request);
+        ///END
 
         return new APIGatewayProxyResponseEvent()
-                .withStatusCode(201)
-                .withBody("this is a body");
+                .withStatusCode(response.getStatus())
+                .withBody(response.getBodyAsString());
     }
 }
